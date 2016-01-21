bd80a0598d197bb0a9e19ecfe79544e80437f8eb
Pavel Safronov <pavel@amazon.com>
2016-01-21 10:45:30 -0800

    Add support for using AWS Certificate Manager certs with CloudFront distributions.

--- modules/AWSPowerShell/Cmdlets/AmazonCloudFront/ClientCmdlet.cs
+++ modules/AWSPowerShell/Cmdlets/AmazonCloudFront/ClientCmdlet.cs
@@ -27,7 +27,7 @@ using Amazon.CloudFront.Model;
 
 namespace Amazon.PowerShell.Cmdlets.CF
 {
-    [AWSClientCmdlet("Amazon CloudFront", "CF", "2016-01-13")]
+    [AWSClientCmdlet("Amazon CloudFront", "CF", "2016-01-28")]
     public abstract class AmazonCloudFrontClientCmdlet : ServiceCmdlet
     {
         protected IAmazonCloudFront Client { get; private set; }
