ffc2de067284ea2b197ef88942a84ccb285e7634
Steve Roberts <strobe@amazon.com>
2016-07-19 14:11:54 -0700

    Add support for managing remote access sessions.

---
+++ modules/AWSPowerShell/Cmdlets/AmazonDeviceFarm/Basic/Get-DFRemoteAccessSession-Cmdlet.cs
@@ -0,0 +1,129 @@
+/*******************************************************************************
+ *  Copyright 2012-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *  this file except in compliance with the License. A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file.
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ * *****************************************************************************
+ *
+ *  AWS Tools for Windows (TM) PowerShell (TM)
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using Amazon.PowerShell.Common;
+using Amazon.Runtime;
+using Amazon.DeviceFarm;
+using Amazon.DeviceFarm.Model;
+
+namespace Amazon.PowerShell.Cmdlets.DF
+{
+    /// <summary>
+    /// Returns a link to a currently running remote access session.
+    /// </summary>
+    [Cmdlet("Get", "DFRemoteAccessSession")]
+    [OutputType("Amazon.DeviceFarm.Model.RemoteAccessSession")]
+    [AWSCmdlet("Invokes the GetRemoteAccessSession operation against AWS Device Farm.", Operation = new[] {"GetRemoteAccessSession"})]
+    [AWSCmdletOutput("Amazon.DeviceFarm.Model.RemoteAccessSession",
+        "This cmdlet returns a RemoteAccessSession object.",
+        "The service call response (type Amazon.DeviceFarm.Model.GetRemoteAccessSessionResponse) can also be referenced from properties attached to the cmdlet entry in the $AWSHistory stack."
+    )]
+    public class GetDFRemoteAccessSessionCmdlet : AmazonDeviceFarmClientCmdlet, IExecutor
+    {
+        
+        #region Parameter Arn
+        /// <summary>
+        /// <para>
+        /// <para>The Amazon Resource Name (ARN) of the remote access session about which you want to
+        /// get session information.</para>
+        /// </para>
+        /// </summary>
+        [System.Management.Automation.Parameter(Position = 0, ValueFromPipeline = true)]
+        public System.String Arn { get; set; }
+        #endregion
+        
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            
+            var context = new CmdletContext
+            {
+                Region = this.Region,
+                Credentials = this.CurrentCredentials
+            };
+            
+            context.Arn = this.Arn;
+            
+            var output = Execute(context) as CmdletOutput;
+            ProcessOutput(output);
+        }
+        
+        #region IExecutor Members
+        
+        public object Execute(ExecutorContext context)
+        {
+            var cmdletContext = context as CmdletContext;
+            // create request
+            var request = new Amazon.DeviceFarm.Model.GetRemoteAccessSessionRequest();
+            
+            if (cmdletContext.Arn != null)
+            {
+                request.Arn = cmdletContext.Arn;
+            }
+            
+            CmdletOutput output;
+            
+            // issue call
+            var client = Client ?? CreateClient(context.Credentials, context.Region);
+            try
+            {
+                var response = CallAWSServiceOperation(client, request);
+                Dictionary<string, object> notes = null;
+                object pipelineOutput = response.RemoteAccessSession;
+                output = new CmdletOutput
+                {
+                    PipelineOutput = pipelineOutput,
+                    ServiceResponse = response,
+                    Notes = notes
+                };
+            }
+            catch (Exception e)
+            {
+                output = new CmdletOutput { ErrorResponse = e };
+            }
+            
+            return output;
+        }
+        
+        public ExecutorContext CreateContext()
+        {
+            return new CmdletContext();
+        }
+        
+        #endregion
+        
+        #region AWS Service Operation Call
+        
+        private static Amazon.DeviceFarm.Model.GetRemoteAccessSessionResponse CallAWSServiceOperation(IAmazonDeviceFarm client, Amazon.DeviceFarm.Model.GetRemoteAccessSessionRequest request)
+        {
+            return client.GetRemoteAccessSession(request);
+        }
+        
+        #endregion
+        
+        internal class CmdletContext : ExecutorContext
+        {
+            public System.String Arn { get; set; }
+        }
+        
+    }
+}
---
+++ modules/AWSPowerShell/Cmdlets/AmazonDeviceFarm/Basic/Stop-DFRemoteAccessSession-Cmdlet.cs
@@ -0,0 +1,144 @@
+/*******************************************************************************
+ *  Copyright 2012-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License"). You may not use
+ *  this file except in compliance with the License. A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ *  or in the "license" file accompanying this file.
+ *  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ *  CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations under the License.
+ * *****************************************************************************
+ *
+ *  AWS Tools for Windows (TM) PowerShell (TM)
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using Amazon.PowerShell.Common;
+using Amazon.Runtime;
+using Amazon.DeviceFarm;
+using Amazon.DeviceFarm.Model;
+
+namespace Amazon.PowerShell.Cmdlets.DF
+{
+    /// <summary>
+    /// Ends a specified remote access session.
+    /// </summary>
+    [Cmdlet("Stop", "DFRemoteAccessSession", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Medium)]
+    [OutputType("Amazon.DeviceFarm.Model.RemoteAccessSession")]
+    [AWSCmdlet("Invokes the StopRemoteAccessSession operation against AWS Device Farm.", Operation = new[] {"StopRemoteAccessSession"})]
+    [AWSCmdletOutput("Amazon.DeviceFarm.Model.RemoteAccessSession",
+        "This cmdlet returns a RemoteAccessSession object.",
+        "The service call response (type Amazon.DeviceFarm.Model.StopRemoteAccessSessionResponse) can also be referenced from properties attached to the cmdlet entry in the $AWSHistory stack."
+    )]
+    public class StopDFRemoteAccessSessionCmdlet : AmazonDeviceFarmClientCmdlet, IExecutor
+    {
+        
+        #region Parameter Arn
+        /// <summary>
+        /// <para>
+        /// <para>The Amazon Resource Name (ARN) of the remote access session you wish to stop.</para>
+        /// </para>
+        /// </summary>
+        [System.Management.Automation.Parameter(Position = 0, ValueFromPipeline = true)]
+        public System.String Arn { get; set; }
+        #endregion
+        
+        #region Parameter Force
+        /// <summary>
+        /// This parameter overrides confirmation prompts to force 
+        /// the cmdlet to continue its operation. This parameter should always
+        /// be used with caution.
+        /// </summary>
+        [System.Management.Automation.Parameter]
+        public SwitchParameter Force { get; set; }
+        #endregion
+        
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            
+            var resourceIdentifiersText = FormatParameterValuesForConfirmationMsg("Arn", MyInvocation.BoundParameters);
+            if (!ConfirmShouldProceed(this.Force.IsPresent, resourceIdentifiersText, "Stop-DFRemoteAccessSession (StopRemoteAccessSession)"))
+            {
+                return;
+            }
+            
+            var context = new CmdletContext
+            {
+                Region = this.Region,
+                Credentials = this.CurrentCredentials
+            };
+            
+            context.Arn = this.Arn;
+            
+            var output = Execute(context) as CmdletOutput;
+            ProcessOutput(output);
+        }
+        
+        #region IExecutor Members
+        
+        public object Execute(ExecutorContext context)
+        {
+            var cmdletContext = context as CmdletContext;
+            // create request
+            var request = new Amazon.DeviceFarm.Model.StopRemoteAccessSessionRequest();
+            
+            if (cmdletContext.Arn != null)
+            {
+                request.Arn = cmdletContext.Arn;
+            }
+            
+            CmdletOutput output;
+            
+            // issue call
+            var client = Client ?? CreateClient(context.Credentials, context.Region);
+            try
+            {
+                var response = CallAWSServiceOperation(client, request);
+                Dictionary<string, object> notes = null;
+                object pipelineOutput = response.RemoteAccessSession;
+                output = new CmdletOutput
+                {
+                    PipelineOutput = pipelineOutput,
+                    ServiceResponse = response,
+                    Notes = notes
+                };
+            }
+            catch (Exception e)
+            {
+                output = new CmdletOutput { ErrorResponse = e };
+            }
+            
+            return output;
+        }
+        
+        public ExecutorContext CreateContext()
+        {
+            return new CmdletContext();
+        }
+        
+        #endregion
+        
+        #region AWS Service Operation Call
+        
+        private static Amazon.DeviceFarm.Model.StopRemoteAccessSessionResponse CallAWSServiceOperation(IAmazonDeviceFarm client, Amazon.DeviceFarm.Model.StopRemoteAccessSessionRequest request)
+        {
+            return client.StopRemoteAccessSession(request);
+        }
+        
+        #endregion
+        
+        internal class CmdletContext : ExecutorContext
+        {
+            public System.String Arn { get; set; }
+        }
+        
+    }
+}
