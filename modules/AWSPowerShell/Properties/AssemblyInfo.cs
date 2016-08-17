e52311a0463c0eb59ec10d15317a40f894faee49
Steve Roberts <strobe@amazon.com>
2016-08-17 09:00:48 -0700

    Recut branch from from 3.1.93.0 in master, enable netcore module build

--- modules/AWSPowerShell/Properties/AssemblyInfo.cs
+++ modules/AWSPowerShell/Properties/AssemblyInfo.cs
@@ -5,9 +5,17 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if DESKTOP
 [assembly: AssemblyTitle("AWS Tools for Windows PowerShell")]
-[assembly: AssemblyCompany("Amazon.com, Inc")]
 [assembly: AssemblyProduct("AWS Tools for Windows PowerShell")]
+#elif CORECLR
+[assembly: AssemblyTitle("AWS Tools for PowerShell Core")]
+[assembly: AssemblyProduct("AWS Tools for PowerShell Core")]
+#else
+#error "Unknown build edition"
+#endif	
+	
+[assembly: AssemblyCompany("Amazon.com, Inc")]
 [assembly: AssemblyCopyright("Copyright 2012-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
