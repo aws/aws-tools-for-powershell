1059d34e46e9bef9c9521bfb65ffe379cd364986
Steve Roberts <strobe@amazon.com>
2016-08-26 13:57:18 -0700

    Dropped smoke designator from filenames since we have tag. Added ClouwWatchLogs test.

---
+++ tests/CloudWatchLogs/CloudWatchLogs.Tests.ps1
@@ -0,0 +1,17 @@
+Describe -Tag "Smoke" "CloudWatchLogs Tests" {
+
+    BeforeEach {
+        Set-AWSCredentials default
+        Set-DefaultAWSRegion us-east-1
+    }
+
+    Context "List log groups" {
+
+        It "Can list log groups" {
+            $logGroups = Get-CWLLogGroups
+            if ($logGroups) {
+                $logGroups.Count | Should BeGreaterThan 0
+            }
+        }
+    }
+}
