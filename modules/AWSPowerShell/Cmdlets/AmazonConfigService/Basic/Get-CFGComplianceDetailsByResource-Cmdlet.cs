28f36f88659932e54589e7b556363badfacbebe1
John Vellozzi <vellozzi@amazon.com>
2017-01-16 21:37:32 -0800

    Recently we added next token implementation to some of our APIs mentioned in the title. As part of that launch we added InvalidNextTokenException to those APIs but forgot to update the model for the APIs. This item is to update the models to include InvalidNextTokenException in API response.

--- modules/AWSPowerShell/Cmdlets/AmazonConfigService/Basic/Get-CFGComplianceDetailsByResource-Cmdlet.cs
+++ modules/AWSPowerShell/Cmdlets/AmazonConfigService/Basic/Get-CFGComplianceDetailsByResource-Cmdlet.cs
@@ -77,7 +77,7 @@ namespace Amazon.PowerShell.Cmdlets.CFG
         #region Parameter NextToken
         /// <summary>
         /// <para>
-        /// <para>The <code>nextToken</code> string returned on a previous page that you use to get
+        /// <para>The <code>NextToken</code> string returned on a previous page that you use to get
         /// the next page of results in a paginated response.</para>
         /// </para>
         /// </summary>
