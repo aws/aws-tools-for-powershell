9ed7c211aad2bb62a37e0aa125baabc1cb46cb4a
Matteo Prosperi <41970398+matteo-prosperi@users.noreply.github.com>
2019-11-22 08:37:07 -0800

    Fixing cmdlet names returned by Get-AWSCmdletName. Adding "Mac" tag to manifests. Removing unused manifest patchers. Fixing typos.

--- tests/Discovery/Get-AWSCmdletName.Tests.ps1
+++ tests/Discovery/Get-AWSCmdletName.Tests.ps1
@@ -21,21 +21,21 @@ Describe -Tag "Smoke" "Get-AWSCmdletName" {
         }
         
         It "should support filtering by assembly name" {
-            (Get-AWSCmdletName -Service 'SimpleSystemsManagement' | Where-Object { $_.CmdletName -eq 'Add-SSMSSMResourceTag' }).Count | Should BeGreaterThan 0
+            (Get-AWSCmdletName -Service 'SimpleSystemsManagement' | Where-Object { $_.CmdletName -eq 'Add-SSMResourceTag' }).Count | Should BeGreaterThan 0
         }
 
         It "should support filtering by service name" {
-            (Get-AWSCmdletName -Service 'AWS Systems Manager' | Where-Object { $_.CmdletName -eq 'Add-SSMSSMResourceTag' }).Count | Should BeGreaterThan 0
+            (Get-AWSCmdletName -Service 'AWS Systems Manager' | Where-Object { $_.CmdletName -eq 'Add-SSMResourceTag' }).Count | Should BeGreaterThan 0
         }
 
         It "should support filtering by service prefix" {
-            (Get-AWSCmdletName -Service 'SSM' | Where-Object { $_.CmdletName -eq 'Add-SSMSSMResourceTag' }).Count | Should BeGreaterThan 0
+            (Get-AWSCmdletName -Service 'SSM' | Where-Object { $_.CmdletName -eq 'Add-SSMResourceTag' }).Count | Should BeGreaterThan 0
         }
 
         It "filtering by service prefix should use strict equality" {
             (Get-AWSCmdletName -Service 'S3' | Where-Object { $_.CmdletName -eq 'Write-S3Object' }).Count | Should BeGreaterThan 0
-            (Get-AWSCmdletName -Service 'S3C' | Where-Object { $_.CmdletName -eq 'Add-S3CS3CPublicAccessBlock' }).Count | Should BeGreaterThan 0
-            (Get-AWSCmdletName -Service 'S3' | Where-Object { $_.CmdletName -eq 'Add-S3CS3CPublicAccessBlock' }).Count | Should BeExactly 0
+            (Get-AWSCmdletName -Service 'S3C' | Where-Object { $_.CmdletName -eq 'Add-S3CPublicAccessBlock' }).Count | Should BeGreaterThan 0
+            (Get-AWSCmdletName -Service 'S3' | Where-Object { $_.CmdletName -eq 'Add-S3CPublicAccessBlock' }).Count | Should BeExactly 0
         }
 
         It "should support filtering by operation and service" {
@@ -47,7 +47,7 @@ Describe -Tag "Smoke" "Get-AWSCmdletName" {
         }
 
         It "should support filtering by cmdlet name" {
-            (Get-AWSCmdletName -CmdletName Write-SSMSSMComplianceItem).Count | Should BeGreaterThan 0
+            (Get-AWSCmdletName -CmdletName Write-SSMComplianceItem).Count | Should BeGreaterThan 0
         }
 
         It "should support filtering by cli command" {
