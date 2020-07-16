ed060eabe2bf56fd93f06f6c4e1db4a2cbc67da2
Philip Pittle <pittle@amazon.com>
2020-07-15 17:12:02 -0700

    moved assembly loader logic into its own class

--- ReleaseNotesGenerator/PSModuleAnalyzer.cs
+++ ReleaseNotesGenerator/PSModuleAnalyzer.cs
@@ -1,11 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
 using System.Management.Automation;
 using System.Reflection;
-using System.Runtime.Loader;
-using Microsoft.Extensions.DependencyModel;
 
 namespace PSReleaseNotesGenerator
 {
@@ -13,49 +10,15 @@ namespace PSReleaseNotesGenerator
     {
         private readonly string _assemblyPath;
 
-        private readonly EmptyAssemblyLoadContext _assemblyLoadContext;
+        private readonly PSModuleAnalyzerAssemblyLoader _assemblyLoadContext;
 
         public PSModuleAnalyzer(string path)
         {
             _assemblyPath = path;
 
-            var assemblyFolder = Path.GetDirectoryName(path);
-
             // use an isolated load context so that multiple instances of the Analyzer (hopefully)
             // wont interfere with each other if, for example, there are different versions of an assembly getting loaded
-            _assemblyLoadContext = new EmptyAssemblyLoadContext();
-
-            _assemblyLoadContext.Resolving += (context, name) =>
-            {
-                // avoid loading *.resources dlls, because of: https://github.com/dotnet/coreclr/issues/8416
-                if (name.Name.EndsWith("resources"))
-                {
-                    return null;
-                }
-
-                var dependencies = DependencyContext.Default.RuntimeLibraries;
-                foreach (var library in dependencies)
-                {
-                    if (IsCandidateLibrary(library, name))
-                    {
-                        return context.LoadFromAssemblyName(new AssemblyName(library.Name));
-                    }
-                }
-
-                var foundDlls = Directory.GetFileSystemEntries(new FileInfo(assemblyFolder).FullName, name.Name + ".dll", SearchOption.AllDirectories);
-                if (foundDlls.Any())
-                {
-                    return context.LoadFromAssemblyPath(foundDlls[0]);
-                }
-
-                return context.LoadFromAssemblyName(name);
-            };
-        }
-
-        private static bool IsCandidateLibrary(RuntimeLibrary library, AssemblyName assemblyName)
-        {
-            return (library.Name == (assemblyName.Name))
-                   || (library.Dependencies.Any(d => d.Name.StartsWith(assemblyName.Name)));
+            _assemblyLoadContext = new PSModuleAnalyzerAssemblyLoader(_assemblyPath);
         }
 
         public IDictionary<string, Cmdlet> Analyze()
@@ -70,28 +33,6 @@ namespace PSReleaseNotesGenerator
                     .ToDictionary(cmdlet => cmdlet.Name, cmdlet => cmdlet);
 
             return new ReadOnlyDictionary<string, Cmdlet>(cmdlets);
-        }
-    }
-
-    /// <summary>
-    /// An isolated <see cref="AssemblyLoadContext"/> inspired by the .net core 3.0 version
-    /// <see cref="AssemblyLoadContext"/> which supports new-ing up a <see cref="AssemblyLoadContext"/>
-    /// directly.
-    /// <para />
-    /// Exclusively meant to support See <see cref="PSModuleAnalyzer"/>.
-    /// </summary>
-    /// <remarks>
-    /// Documentation on assembly resolution sequence:
-    /// https://docs.microsoft.com/en-us/dotnet/api/system.runtime.loader.assemblyloadcontext.loadfromassemblyname?view=netcore-2.1
-    /// </remarks>
-    internal class EmptyAssemblyLoadContext : AssemblyLoadContext
-    {
-        public EmptyAssemblyLoadContext(){}
-
-        protected override Assembly Load(AssemblyName assemblyName)
-        {
-            // return null to continue the assembly resolution sequence.
-            return null;
         }
     }
 }
---
+++ ReleaseNotesGenerator/PSModuleAnalyzerAssemblyLoader.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.Extensions.DependencyModel;
+
+namespace PSReleaseNotesGenerator
+{
+    /// <summary>
+    /// An isolated <see cref="AssemblyLoadContext"/> meant to support See <see cref="PSModuleAnalyzer"/>.
+    /// </summary>
+    /// <remarks>
+    /// Documentation on assembly resolution sequence:
+    /// https://docs.microsoft.com/en-us/dotnet/api/system.runtime.loader.assemblyloadcontext.loadfromassemblyname?view=netcore-2.1
+    /// </remarks>
+    public class PSModuleAnalyzerAssemblyLoader : AssemblyLoadContext
+    {
+        private readonly string _assemblyFolder;
+
+        public PSModuleAnalyzerAssemblyLoader(string assemblyPath)
+        {
+            _assemblyFolder = Path.GetDirectoryName(assemblyPath);
+        }
+
+        /// <remarks>
+        /// return null to continue the assembly load sequance
+        /// </remarks>
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            if (null == assemblyName?.Name)
+                return null;
+
+            // avoid loading *.resources dlls, because of: https://github.com/dotnet/coreclr/issues/8416
+            if (assemblyName.Name.EndsWith("resources"))
+            {
+                return null;
+            }
+
+            var dependencies = DependencyContext.Default.RuntimeLibraries;
+            foreach (var library in dependencies)
+            {
+                if (IsCandidateLibrary(library, assemblyName))
+                {
+                    return null;
+                }
+            }
+
+            var foundDlls = Directory.GetFileSystemEntries(new FileInfo(_assemblyFolder).FullName, assemblyName.Name + ".dll", SearchOption.AllDirectories);
+            if (foundDlls.Any())
+            {
+                return base.LoadFromAssemblyPath(foundDlls[0]);
+            }
+
+            return null;
+        }
+
+        private static bool IsCandidateLibrary(RuntimeLibrary library, AssemblyName assemblyName)
+        {
+            return (library.Name == (assemblyName.Name))
+                   || (library.Dependencies.Any(d => d.Name.StartsWith(assemblyName.Name)));
+        }
+    }
+}
