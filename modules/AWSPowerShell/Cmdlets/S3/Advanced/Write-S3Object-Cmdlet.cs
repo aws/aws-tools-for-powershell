556043ed234cb5d427de37a3d0452ad01baefa64
Jonathan Nunn <jonatnun@amazon.com>
2024-07-15 21:45:04 -0700

    Added properties for alternate methods

--- modules/AWSPowerShell/Cmdlets/S3/Advanced/Write-S3Object-Cmdlet.cs
+++ modules/AWSPowerShell/Cmdlets/S3/Advanced/Write-S3Object-Cmdlet.cs
@@ -697,6 +697,8 @@ namespace Amazon.PowerShell.Cmdlets.S3
                     request.TagSet = new List<Tag>(cmdletContext.TagSet);
                 if (cmdletContext.ChecksumAlgorithm != null)
                     request.ChecksumAlgorithm = cmdletContext.ChecksumAlgorithm;
+                if (!string.IsNullOrEmpty(cmdletContext.IfNoneMatch))
+                    request.IfNoneMatch = cmdletContext.IfNoneMatch;
 
                 request.CalculateContentMD5Header = cmdletContext.CalculateContentMD5Header;
 
