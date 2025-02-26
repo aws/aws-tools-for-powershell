8fdb08363ecccf2e87364e3da7d5d06b97cf793f
Brad Knowles <bradkno@amazon.com>
2025-02-25 22:35:42 -0500

    style: 🎨 Refactor Get-Ec2Instance samples with splatting and new lines

--- .doc_gen/snippet_files/ec2_Get-EC2Instance.5.1.ps1
+++ .doc_gen/snippet_files/ec2_Get-EC2Instance.5.1.ps1
@@ -1,3 +1,17 @@
-(Get-EC2Instance -Filter @{Name="instance-state-name";Values=@("running","stopped")}).Instances `
- | Select InstanceID,InstanceType,Platform,PrivateIpAddress,@{Name="Name";Expression={$_.Tags[$_.Tags.Key.IndexOf("Name")].Value}},@{N="State";E={$_.State.Name}} `
- | Format-Table
+$InstanceParams = @{
+    Filter = @(
+        @{'Name' = 'instance-state-name';'Values' = @("running","stopped")}
+    )
+}
+
+$SelectParams = @{
+    Property = @(
+        "InstanceID", "InstanceType", "Platform", "PrivateIpAddress",
+        @{Name="Name";Expression={$_.Tags[$_.Tags.Key.IndexOf("Name")].Value}},
+        @{Name="State";Expression={$_.State.Name}}
+    )
+}
+
+(Get-EC2Instance @InstanceParams).Instances `
+ | Select @SelectParams `
+ | Format-Table -AutoSize
