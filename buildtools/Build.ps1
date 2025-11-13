ef600f9a8a44ec112f900b76ecd2d7fd5c8e91ea
Ashish Dhingra <67916761+ashishdhingra@users.noreply.github.com>
2025-11-13 13:15:51 -0800

    Temporary disabled removing Core dlls in preview builds

--- buildtools/Build.ps1
+++ buildtools/Build.ps1
@@ -38,8 +38,7 @@ function DownloadSdkArtifacts {
     [string]
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
-    $SdkArtifactsUri,
-    [switch]$SkipAWSSDKCoreDlls
+    $SdkArtifactsUri
   )
   
   if ($SdkArtifactsUri.Trim().EndsWith('_sdk-versions.json')) {
@@ -75,9 +74,6 @@ function DownloadSdkArtifacts {
     }
     Write-Host "Extracting sdk.zip"
     Expand-Archive ./Include/sdk.zip -DestinationPath ./Include/sdktmp -Force
-    if ($SkipAWSSDKCoreDlls) {
-      Remove-Item ./Include/sdktmp/assemblies/*/AWSSDK.Core.*
-    }
     Move-Item ./Include/sdktmp/assemblies ./Include/sdk/assemblies
     Remove-Item ./Include/sdktmp -Recurse
   }
@@ -117,7 +113,7 @@ try {
   if ($BuildType -eq 'PREVIEW') {
     # for Preview build, $SdkArtifactsUri will have s3 uri for the preview artifacts from .NET build. e.g https://bucketname.s3.us-west-2.amazonaws.com/{path to dotnet3.zip}
     if ($SdkArtifactsUri) {
-      DownloadSdkArtifacts -SdkArtifactsUri $SdkArtifactsUri -SkipAWSSDKCoreDlls
+      DownloadSdkArtifacts -SdkArtifactsUri $SdkArtifactsUri
     }
     elseif ($Environment -eq "DEV") {
       Write-Host "WARNING: running preview build without specific SDK artifacts."
