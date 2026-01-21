bd82c384d12f464a1eeb4a3e94b7bfa4e6ed856f
Ashish Dhingra <67916761+ashishdhingra@users.noreply.github.com>
2026-01-21 13:36:39 -0800

    Corrected documentation for Read-S3Object and Set-DDBBatchItem.

--- .doc_gen/snippet_files/dynamodb_Set-DDBBatchItem.1.1.ps1
+++ .doc_gen/snippet_files/dynamodb_Set-DDBBatchItem.1.1.ps1
@@ -1,11 +1,18 @@
 $item = @{
     SongTitle = 'Somewhere Down The Road'
     Artist = 'No One You Know'
-        AlbumTitle = 'Somewhat Famous'
-        Price = 1.94
-        Genre = 'Country'
-        CriticRating = 10.0
+    AlbumTitle = 'Somewhat Famous'
+    Price = 1.94
+    Genre = 'Country'
+    CriticRating = 10.0
 } | ConvertTo-DDBItem
 
 $writeRequest = New-Object Amazon.DynamoDBv2.Model.WriteRequest
 $writeRequest.PutRequest = [Amazon.DynamoDBv2.Model.PutRequest]$item
+
+$requestItem = @{
+    'Music' = [Amazon.DynamoDBv2.Model.WriteRequest]($writeRequest)
+    'Songs' = [Amazon.DynamoDBv2.Model.WriteRequest]($writeRequest)
+}
+
+Set-DDBBatchItem -RequestItem $requestItem
